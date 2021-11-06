d75f4e0cbc608852590bac9c43057d34cf54ecda
Florian CHEVALIER <gassenbube@gmail.com>
2021-11-06 13:07:16 +0100

    fix random opening in walls

--- game.lua
+++ game.lua
@@ -4,6 +4,7 @@ local wall = require("wall")
 local walls = {}
 local distance_between_walls = 500
 local number_of_walls = 15
+local walls_opening_height = {}
 
 wall_x_scale_factor = 2
 
@@ -12,6 +13,8 @@ function game.load()
 
     for i=0, number_of_walls - 1 do
         walls[i] = wall:create(love.graphics.getWidth() + i * distance_between_walls)
+        math.randomseed(os.time()-i)
+        walls_opening_height[i] = math.random(0, love.graphics.getHeight() - texture_opening:getHeight())
     end
 
 end
@@ -19,7 +22,7 @@ end
 function game.draw()
     sugar_glider.draw()
     for i=0, number_of_walls - 1 do
-        wall:draw(walls[i])
+        wall:draw(walls[i], walls_opening_height[i])
     end
 end
 
@@ -31,9 +34,12 @@ function game.update(dt)
         if walls[i].x < 0 - (walls[i].top.img:getWidth() * wall_x_scale_factor) then
             for j=i+1, number_of_walls - 1 do
                 walls[j-1] = walls[j]
+                walls_opening_height[j-1] = walls_opening_height[j]
             end
             local new_wall = wall:create(walls[number_of_walls - 2].x + distance_between_walls)
             walls[number_of_walls - 1] = new_wall
+            math.randomseed(os.time())
+            walls_opening_height[number_of_walls - 1] = math.random(0, love.graphics.getHeight() - texture_opening:getHeight() / 2)
         end
     end
 end
--- wall.lua
+++ wall.lua
@@ -1,10 +1,11 @@
 local wall = {}
 local base_speed = 200
 local texture_wall = love.graphics.newImage('textures/Mur_test.png')
-local texture_opening = love.graphics.newImage('textures/Arche_test.png')
 local top = {}
 local opening = {}
 local bottom = {}
+
+texture_opening = love.graphics.newImage('textures/Arche_test.png')
 
 function wall:create(x)
     local new_wall = {}
@@ -16,10 +17,10 @@ function wall:create(x)
 
     opening.img = texture_opening
     math.randomseed(os.time())
-    opening.y = math.random(0, love.graphics.getHeight() - opening.img:getHeight())
+    opening.y = math.random(0, love.graphics.getHeight() - opening.img:getHeight() / 2)
 
     bottom.img = texture_wall
-    bottom.y = opening.y + opening.img:getHeight() 
+    -- bottom.y = opening.y + opening.img:getHeight()
 
     new_wall.top = top
     new_wall.opening = opening
@@ -28,11 +29,11 @@ function wall:create(x)
     return new_wall
 end
 
-function wall:draw(wall_to_draw)
-    local y_scale_factor = wall_to_draw.opening.y / wall_to_draw.top.img:getHeight() + 0.1
+function wall:draw(wall_to_draw, y_opening)
+    local y_scale_factor = y_opening / wall_to_draw.top.img:getHeight() + 0.1
     love.graphics.draw(wall_to_draw.top.img, wall_to_draw.x, wall_to_draw.top.y, 0, wall_x_scale_factor, y_scale_factor)
-    love.graphics.draw(wall_to_draw.opening.img, wall_to_draw.x, wall_to_draw.opening.y - wall_to_draw.opening.img:getHeight() / 2, 0, wall_x_scale_factor)
-    love.graphics.draw(wall_to_draw.bottom.img, wall_to_draw.x, wall_to_draw.bottom.y, 0, wall_x_scale_factor, 10)
+    love.graphics.draw(wall_to_draw.opening.img, wall_to_draw.x, y_opening - wall_to_draw.opening.img:getHeight() / 2, 0, wall_x_scale_factor)
+    love.graphics.draw(wall_to_draw.bottom.img, wall_to_draw.x, y_opening + opening.img:getHeight(), 0, wall_x_scale_factor, 10)
 end
 
 function wall:update(dt)

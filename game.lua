a356c88d999ba96bdbeb968027e6b4144f9e3735
Florian CHEVALIER <gassenbube@gmail.com>
2021-11-07 17:23:02 +0100

    wallah (#6)
    ajout des menus de fin et de pause
    ajout du score
    travail de positionnement et de design
    
    Co-authored-by: Bringer Thomas <nobuta.tb@gmail.com>

--- game.lua
+++ game.lua
@@ -1,18 +1,22 @@
 local game = {}
 local sugar_glider = require("sugar_glider")
 local wall = require("wall")
+local suit = require "libraries.suit"
+local text_font = love.graphics.newFont("fonts/upheavtt.ttf", 20)
+local score_font = love.graphics.newFont("fonts/QuantumProfit.ttf", 20)
 local walls = {}
 local distance_between_walls = 750
 local number_of_walls = 8
 local walls_vertical_speed = 100
 local walls_opening_height = {}
+score = 0
 local design = require("design")
 
 wall_x_scale_factor = 2
 
 function game.load()
     sugar_glider.load()
-
+    score = 0
     for i=0, number_of_walls - 1 do
         -- walls[i] = wall:create(love.graphics.getWidth() + i * distance_between_walls)
         walls[i] = wall:create(love.graphics.getWidth() + i * distance_between_walls)
@@ -31,11 +35,16 @@ function game.draw()
     for i=0, number_of_walls - 1 do
         wall:draw_foreground(walls[i], walls_opening_height[i])
     end
+    suit.layout:reset(10,10)
+    suit.layout:padding(10,10)
+    suit.Label("Score :", {font = text_font}, suit.layout:row(100,30))
+    suit.Label(score, {font = score_font}, suit.layout:col(100,30))
+    suit.draw()
 end
 
 function game.update(dt)
     sugar_glider.move(dt)
-
+    score = score + 1
     for i=0, number_of_walls - 1 do
         walls[i].x = walls[i].x - walls[i].speed * dt
         if walls[i].x < 0 - (walls[i].top.img:getWidth() * wall_x_scale_factor) then
@@ -52,7 +61,8 @@ function game.update(dt)
         -- collisions
         if (sugar_glider.y > walls[i].x and sugar_glider.x < walls[i].x + texture_opening:getWidth()) or (sugar_glider.x + sugar_glider.img:getWidth() * 5 > walls[i].x and sugar_glider.x + sugar_glider.img:getWidth() * 5 < walls[i].x + texture_opening:getWidth()) then
             if sugar_glider.y < walls_opening_height[i] or sugar_glider.y > walls_opening_height[i] + texture_opening:getHeight() then
-                love.event.quit()
+                final_score = score
+                state = "end"
             end
         end
     end
@@ -71,7 +81,7 @@ function game.update(dt)
     -- end
 
     if love.keyboard.isDown('escape') then
-        love.event.quit()
+        state = "pause"
     end
 end
 
--- main.lua
+++ main.lua
@@ -2,19 +2,27 @@ local game = require("game")
 local menu = require("menus")
 -- love.window.setFullscreen(true)
 love.window.setMode(0,0)
+state = "start"
 
 function love.load()
-    game.load()
+  game.load()
 end
 
 function love.draw()
-    if menu.start_menu() then
-        game.draw()
+    if state == "start" then
+      menu.start_menu()
+    elseif state == "game" then
+      game.draw()
+    elseif state == "pause" then
+      menu.pause_menu(score)
+    elseif state == "end" then
+      menu.end_menu(final_score)
+      game.load()
     end
 end
 
 function love.update(dt)
-    if menu.start_menu() then
-        game.update(dt)
-    end
+  if state == "game" then
+      game.update(dt)
+  end
 end
